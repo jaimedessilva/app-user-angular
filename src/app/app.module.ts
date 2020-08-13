a737118a611ffda5a9c2140d3e560841fad40761
Jaime Silva <jaimedessilva@gmail.com>
2020-08-13 03:53:39 -0300

    ultima atualização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; /* Material Toolbar */
 import { MatIconModule } from '@angular/material/icon'; /* Material Icons */
 import { MatButtonModule } from '@angular/material/button'; /* material Button */
-//import { UserListComponent } from './views/home/user-list/user-list.component'; /*Componente List*/
 import { MatSidenavModule } from '@angular/material/sidenav'; /*Material SideNav*/
-import { MatListModule } from '@angular/material/list';
-import { HeaderComponent } from './templates/header/header.component';
-import { FooterComponent } from './templates/footer/footer.component'; /*Material Lista */
+import { MatListModule } from '@angular/material/list'; /*Lista */
+import { HeaderComponent } from './templates/header/header.component'; /* Header */
+import { FooterComponent } from './templates/footer/footer.component'; /* Footer */
 
 
 @NgModule({
